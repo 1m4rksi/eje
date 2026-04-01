6f4e3d2e79fc438c04c7c80a4c79f0017dcfdeaa
Alumno <alumno@et26.local>
2026-04-01 15:15:12 -0300

    ejercicio1

---
+++ index.js
@@ -0,0 +1,7 @@
+function ejercicio1(pareas){ 
+const meses = [ "Enero ", "Febrero ", "Marzo ", "abril ", "mayo ", "Junio ", "julio ", "agosto ", "septiembre ", "octubre ", "noviembre ", "diciembre " ] 
+
+ console.log(meses);
+  
+}
+
